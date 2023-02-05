e67734d2017e169f9d0e5934ba0d2f86224f41af
Seok Yim(Noah) <seokyim8@gmail.com>
2023-02-05 07:18:02 -0500

    Made very small changes to gradle files to avoid using deprecated tools.

--- desktop/src/com/tetris/game/DesktopLauncher.java
+++ desktop/src/com/tetris/game/DesktopLauncher.java
@@ -6,6 +6,7 @@ import com.tetris.game.TetrisGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
